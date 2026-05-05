9eec56d0bb1c1ef32f9a968bd22e643a5e606703
AntonioJCS2025 <jadersonc.s@hotmail.com>
2026-05-04 22:05:36 -0300

    04-05-2026
    Refatoração do cód segundo arq MVC.

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const userController = require("./controllers/userController")
+
+const app = express()
+
+function callbackDaRaiz( request, response ) {
+    response.send("VAI CORINTHIANS!!!! 🦅")
+}
+
+app.get("/", callbackDaRaiz)
+
+app.get("/users", userController.getAllUsers)
+
+module.exports = app
+''
---
+++ src/models/userModel.js
@@ -0,0 +1,11 @@
+const usersList = require("../data/users")
+
+    const findAll = () => {
+        return usersList
+
+    }
+
+    module.exports = {
+        findAll
+
+    }
